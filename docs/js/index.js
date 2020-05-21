5ec5a9476dc0fd60f113cdd2cef1ae1d36e5451f
Jakub Rekowski <jakub.rekowski@gmail.com>
2020-05-21 16:38:19 +0200

    Move demo to preview.unoff.pl

--- docs/js/index.js
+++
@@ -1,7 +0,0 @@
-let jsonInput = document.querySelector('#json-input')
-let jsonInputBtn = document.querySelector('#json-input-btn')
-
-jsonInputBtn.addEventListener('click', (event) => {
-    document.querySelector('#output-code').innerHTML = interpreter(jsonInput.value) + '<br><pre class="text-bold">HTML source is in console</pre>'
-    console.log(interpreter(jsonInput.value))
-})
